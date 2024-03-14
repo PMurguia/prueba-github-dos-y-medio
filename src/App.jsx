772cf71a0eddd0d9729cec5786b52cd4a28c8997
Brian051 <brian051.work@gmail.com>
2024-03-14 12:24:44 +0100

    He metido mi componente en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Saludo from './Saludo'
 import './App.css'
+import Brian from './Brian'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <Saludo />
+    <Brian/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
