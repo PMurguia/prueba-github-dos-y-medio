60b9a88af6b789e33a836ca6a3160eed6cddb630
MiguelEiz <sr.turnerr@gmail.com>
2024-03-14 12:24:55 +0100

    Using component Miguel in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Saludo from './Saludo'
 import './App.css'
+import Miguel from './Miguel'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <Saludo />
+    <Miguel />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
