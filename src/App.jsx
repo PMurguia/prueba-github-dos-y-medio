2351d7a3e79c1f827596578f6302b62c84f50be4
saioamurillo <saioamurillo2.0@gmail.com>
2024-03-14 12:25:25 +0100

    using component Saioa in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Saludo from './Saludo'
 import './App.css'
+import Saioa from './Saioa'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,26 +11,9 @@ function App() {
   return (
     <>
     <Saludo />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Saioa />
+
+     
     </>
   )
 }
